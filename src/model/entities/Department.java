0b78bc38f93669a8300802025ffc811bde919917
Júlio César <julnhoo.cesarcontreras@gmail.com>
2024-07-03 14:16:36 -0300

    Acessando o banco de dados

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -4,15 +4,16 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Department implements Serializable{
-
+	
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String name;
 	
 	public Department() {
 		
 	}
-
+	
 	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
@@ -50,9 +51,10 @@ public class Department implements Serializable{
 		Department other = (Department) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
+	
 }
